358b460fdf8be5402faf0c21854e666d13d1fe12
Eron Gjoni <yegjoni@worcester.edu>
2023-12-29 10:02:23 +0000

    added setup file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DRUGS",
+    version="0.0.1", 
+    author="Eron Gjoni",
+    author_email="rufsketch1@gmail.com",
+    description="A transformers module for increasing model output diversity by noise injection",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown", 
+    url="https://github.com/EGjoni/DRUGS",
+    package_dir={"": "drugs"},
+    packages=find_packages(where="drugs"),
+    install_requires=[
+        "transformers", 
+        "torch",
+        "numpy",
+        "json",
+        "msgpack"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
